7525002a2d7261612a96ff335a390c9b152fb332
RanchoDad <joshuagarber215@gmail.com>
2023-05-08 17:54:25 -0700

    initial commit

---
+++ head_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from .models import Guitar
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def guitars_index(request):
+    guitars = Guitar.objects.all()
+    return render(request, 'guitars/index.html', {
+        'guitars': guitars
+    })
+
+def guitars_detail(request, guitar_id):
+    guitar = Guitar.objects.get(id=guitar_id)
+    return render(request, 'guitars/detail.html', {
+        'guitar': guitar
+    })
