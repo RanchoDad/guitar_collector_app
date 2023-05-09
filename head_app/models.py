1f7933365a0579b5b5872ffb31be34b7c8de29ef
RanchoDad <joshuagarber215@gmail.com>
2023-05-09 15:11:32 -0700

    add crud and forms

--- head_app/models.py
+++ head_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class Guitar(models.Model):
@@ -7,8 +9,8 @@ class Guitar(models.Model):
   description = models.TextField(max_length=250)
   year = models.IntegerField()
 
-  # Changing this instance method
-  # does not impact the database, therefore
-  # no makemigrations is necessary
   def __str__(self):
     return f'{self.name} ({self.id})'
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'guitar_id': self.id})
